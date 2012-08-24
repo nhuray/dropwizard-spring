f49f3fa95ffa3ba83b5151d79124075233f95ae1
nhuray <nicolas.huray@figarocms.fr>
2012-08-24 16:02:40 +0200

    wip

--- src/main/java/com.github/nhuray/dropwizard/spring/DropwizardContext.java
+++ src/main/java/com.github/nhuray/dropwizard/spring/DropwizardContext.java
@@ -1,28 +1,31 @@
 package com.github.nhuray.dropwizard.spring;
 
 import com.yammer.dropwizard.config.Configuration;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.StaticApplicationContext;
 
 /**
  * {@link org.springframework.context.ApplicationContext} implementation to register Dropwizard {@link Configuration} as a Spring bean.
  * <p/>
  * The name of the Dropwizard {@link Configuration} bean is 'dw'.
  * <p/>
- *
  */
 public class DropwizardContext extends AnnotationConfigApplicationContext {
 
     /**
-	 * Name of the Dropwizard {@link Configuration} bean in the factory.
-	 */
+     * Name of the Dropwizard {@link Configuration} bean in the factory.
+     */
     public static final String DROPWIZARD_CONFIGURATION = "dw";
 
     public DropwizardContext(Configuration configuration) {
         // Register dropwizard configuration
         getBeanFactory().registerSingleton(DROPWIZARD_CONFIGURATION, configuration);
-        refresh();
 
+        BeanDefinition beanDefinition = new RootBeanDefinition();
+        beanDefinition.setBeanClassName(configuration.getClass().getName());
+        registerBeanDefinition(configuration.getClass().getName(), beanDefinition);
+        refresh();
 
     }
 }
